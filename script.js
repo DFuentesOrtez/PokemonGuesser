bfa325f3280c9200303d4cf724325f1cebefed28
DFuentesOrtez <fuentesortezdavid@gmail.com>
2022-11-21 11:21:56 -0500

    Added all first gen pokemon

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function showImage(src) {
 
 const reveal = document.querySelector("#reveal");
 reveal.addEventListener("click", function () {
+    answerContainer.replaceChildren();
     container.appendChild(imgName);
 });
 
@@ -58,7 +59,10 @@ var guess = document.querySelector("#guess");
 submit.addEventListener("click", function () {
     var myGuess = guess.value;
     guess.value = "";
-    if ((myGuess).toUpperCase() == currentPoke.toUpperCase()) {
+    if (myGuess.toUpperCase() == currentPoke.toUpperCase() ||
+        (currentPoke.toUpperCase() == "mrmime".toUpperCase() && (((myGuess.toUpperCase() == "mr. mime".toUpperCase())) || (myGuess.toUpperCase() == "mr mime".toUpperCase()))) ||
+        (currentPoke.toUpperCase() == "farfetchd".toUpperCase() && (myGuess.toUpperCase() == "farfetch'd".toUpperCase())) ||
+        ((currentPoke.toUpperCase() == "nidoran(f)".toUpperCase() || currentPoke.toUpperCase() == "nidoran(m)".toUpperCase()) && (myGuess.toUpperCase() == "nidoran".toUpperCase()))) {
         answerContainer.replaceChildren();
         var answer = document.createElement("p");
         answer.textContent = "Correct!"
@@ -79,7 +83,16 @@ restart.addEventListener("click", function () {
     container.replaceChildren();
     answerContainer.replaceChildren();
     guess.value = "";
-    allPoke = [poke1, poke2, poke3, poke4, poke5, poke6, poke7, poke8, poke9];
+    imgName.textContent = "";
+    allPoke = [poke1, poke2, poke3, poke4, poke5, poke6, poke7, poke8, poke9, poke10, poke11, poke12, poke13, poke14, poke15, poke16, poke17, poke18, poke19,
+        poke20, poke21, poke22, poke23, poke24, poke25, poke26, poke27, poke28, poke29, poke30, poke31, poke32, poke33, poke34, poke35, poke36, poke37,
+        poke38, poke39, poke40, poke41, poke42, poke43, poke44, poke45, poke46, poke47, poke48, poke49, poke50, poke51, poke52, poke53, poke54, poke55,
+        poke56, poke57, poke58, poke59, poke60, poke61, poke62, poke63, poke64, poke65, poke66, poke67, poke68, poke69, poke70, poke71, poke72, poke73,
+        poke74, poke75, poke76, poke77, poke78, poke79, poke80, poke81, poke82, poke83, poke84, poke85, poke86, poke87, poke88, poke89, poke90, poke91,
+        poke92, poke93, poke94, poke95, poke96, poke97, poke98, poke99, poke100, poke101, poke102, poke103, poke104, poke105, poke106, poke107, poke108,
+        poke109, poke110, poke111, poke112, poke113, poke114, poke115, poke116, poke117, poke118, poke119, poke120, poke121, poke122, poke123, poke124,
+        poke125, poke126, poke127, poke128, poke129, poke130, poke131, poke132, poke133, poke134, poke135, poke136, poke137, poke138, poke139, poke140,
+        poke141, poke142, poke143, poke144, poke145, poke146, poke147, poke148, poke149, poke150, poke151];
 });
 
 
@@ -110,6 +123,439 @@ poke8.value = "images/wartortle.png";
 let poke9 = document.createElement("img");
 poke9.value = "images/blastoise.png";
 
+let poke10 = document.createElement("img");
+poke10.value = "images/caterpie.png";
+
+let poke11 = document.createElement("img");
+poke11.value = "images/metapod.png";
+
+let poke12 = document.createElement("img");
+poke12.value = "images/butterfree.png";
+
+let poke13 = document.createElement("img");
+poke13.value = "images/weedle.png";
+
+let poke14 = document.createElement("img");
+poke14.value = "images/metapod.png";
+
+let poke15 = document.createElement("img");
+poke15.value = "images/beedrill.png";
+
+let poke16 = document.createElement("img");
+poke16.value = "images/pidgey.png";
+
+let poke17 = document.createElement("img");
+poke17.value = "images/pidgeotto.png";
+
+let poke18 = document.createElement("img");
+poke18.value = "images/pidgeot.png";
+
+let poke19 = document.createElement("img");
+poke19.value = "images/rattata.png";
+
+let poke20 = document.createElement("img");
+poke20.value = "images/raticate.png";
+
+let poke21 = document.createElement("img");
+poke21.value = "images/spearow.png";
+
+let poke22 = document.createElement("img");
+poke22.value = "images/fearow.png";
+
+let poke23 = document.createElement("img");
+poke23.value = "images/ekans.png";
+
+let poke24 = document.createElement("img");
+poke24.value = "images/arbok.png";
+
+let poke25 = document.createElement("img");
+poke25.value = "images/pikachu.png";
+
+let poke26 = document.createElement("img");
+poke26.value = "images/raichu.png";
+
+let poke27 = document.createElement("img");
+poke27.value = "images/sandshrew.png";
+
+let poke28 = document.createElement("img");
+poke28.value = "images/sandslash.png";
+
+let poke29 = document.createElement("img");
+poke29.value = "images/nidoran(f).png";
+
+let poke30 = document.createElement("img");
+poke30.value = "images/nidorina.png";
+
+let poke31 = document.createElement("img");
+poke31.value = "images/nidoqueen.png";
+
+let poke32 = document.createElement("img");
+poke32.value = "images/nidoran(m).png";
+
+let poke33 = document.createElement("img");
+poke33.value = "images/nidorino.png";
+
+let poke34 = document.createElement("img");
+poke34.value = "images/nidoking.png";
+
+let poke35 = document.createElement("img");
+poke35.value = "images/clefairy.png";
+
+let poke36 = document.createElement("img");
+poke36.value = "images/clefable.png";
+
+let poke37 = document.createElement("img");
+poke37.value = "images/vulpix.png";
+
+let poke38 = document.createElement("img");
+poke38.value = "images/ninetales.png";
+
+let poke39 = document.createElement("img");
+poke39.value = "images/jigglypuff.png";
+
+let poke40 = document.createElement("img");
+poke40.value = "images/wigglytuff.png";
+
+let poke41 = document.createElement("img");
+poke41.value = "images/zubat.png";
+
+let poke42 = document.createElement("img");
+poke42.value = "images/golbat.png";
+
+let poke43 = document.createElement("img");
+poke43.value = "images/oddish.png";
+
+let poke44 = document.createElement("img");
+poke44.value = "images/gloom.png";
+
+let poke45 = document.createElement("img");
+poke45.value = "images/vileplume.png";
+
+let poke46 = document.createElement("img");
+poke46.value = "images/paras.png";
+
+let poke47 = document.createElement("img");
+poke47.value = "images/parasect.png";
+
+let poke48 = document.createElement("img");
+poke48.value = "images/venonat.png";
+
+let poke49 = document.createElement("img");
+poke49.value = "images/venomoth.png";
+
+let poke50 = document.createElement("img");
+poke50.value = "images/diglett.png";
+
+let poke51 = document.createElement("img");
+poke51.value = "images/dugtrio.png";
+
+let poke52 = document.createElement("img");
+poke52.value = "images/meowth.png";
+
+let poke53 = document.createElement("img");
+poke53.value = "images/persian.png";
+
+let poke54 = document.createElement("img");
+poke54.value = "images/psyduck.png";
+
+let poke55 = document.createElement("img");
+poke55.value = "images/golduck.png";
+
+let poke56 = document.createElement("img");
+poke56.value = "images/mankey.png";
+
+let poke57 = document.createElement("img");
+poke57.value = "images/primeape.png";
+
+let poke58 = document.createElement("img");
+poke58.value = "images/growlithe.png";
+
+let poke59 = document.createElement("img");
+poke59.value = "images/arcanine.png";
+
+let poke60 = document.createElement("img");
+poke60.value = "images/poliwag.png";
+
+let poke61 = document.createElement("img");
+poke61.value = "images/poliwhirl.png";
+
+let poke62 = document.createElement("img");
+poke62.value = "images/poliwrath.png";
+
+let poke63 = document.createElement("img");
+poke63.value = "images/abra.png";
+
+let poke64 = document.createElement("img");
+poke64.value = "images/kadabra.png";
+
+let poke65 = document.createElement("img");
+poke65.value = "images/alakazam.png";
+
+let poke66 = document.createElement("img");
+poke66.value = "images/machop.png";
+
+let poke67 = document.createElement("img");
+poke67.value = "images/machoke.png";
+
+let poke68 = document.createElement("img");
+poke68.value = "images/machamp.png";
+
+let poke69 = document.createElement("img");
+poke69.value = "images/bellsprout.png";
+
+let poke70 = document.createElement("img");
+poke70.value = "images/weepinbell.png";
+
+let poke71 = document.createElement("img");
+poke71.value = "images/victreebel.png";
+
+let poke72 = document.createElement("img");
+poke72.value = "images/tentacool.png";
+
+let poke73 = document.createElement("img");
+poke73.value = "images/tentacruel.png";
+
+let poke74 = document.createElement("img");
+poke74.value = "images/geodude.png";
+
+let poke75 = document.createElement("img");
+poke75.value = "images/graveler.png";
+
+let poke76 = document.createElement("img");
+poke76.value = "images/golem.png";
+
+let poke77 = document.createElement("img");
+poke77.value = "images/ponyta.png";
+
+let poke78 = document.createElement("img");
+poke78.value = "images/rapidash.png";
+
+let poke79 = document.createElement("img");
+poke79.value = "images/slowpoke.png";
+
+let poke80 = document.createElement("img");
+poke80.value = "images/slowbro.png";
+
+let poke81 = document.createElement("img");
+poke81.value = "images/magnemite.png";
+
+let poke82 = document.createElement("img");
+poke82.value = "images/magneton.png";
+
+let poke83 = document.createElement("img");
+poke83.value = "images/farfetchd.png";
+
+let poke84 = document.createElement("img");
+poke84.value = "images/doduo.png";
+
+let poke85 = document.createElement("img");
+poke85.value = "images/dodrio.png";
+
+let poke86 = document.createElement("img");
+poke86.value = "images/seel.png";
+
+let poke87 = document.createElement("img");
+poke87.value = "images/dewgong.png";
+
+let poke88 = document.createElement("img");
+poke88.value = "images/grimer.png";
+
+let poke89 = document.createElement("img");
+poke89.value = "images/muk.png";
+
+let poke90 = document.createElement("img");
+poke90.value = "images/shellder.png";
+
+let poke91 = document.createElement("img");
+poke91.value = "images/cloyster.png";
+
+let poke92 = document.createElement("img");
+poke92.value = "images/gastly.png";
+
+let poke93 = document.createElement("img");
+poke93.value = "images/haunter.png";
+
+let poke94 = document.createElement("img");
+poke94.value = "images/gengar.png";
+
+let poke95 = document.createElement("img");
+poke95.value = "images/onix.png";
+
+let poke96 = document.createElement("img");
+poke96.value = "images/drowzee.png";
+
+let poke97 = document.createElement("img");
+poke97.value = "images/hypno.png";
+
+let poke98 = document.createElement("img");
+poke98.value = "images/krabby.png";
+
+let poke99 = document.createElement("img");
+poke99.value = "images/kingler.png";
+
+let poke100 = document.createElement("img");
+poke100.value = "images/voltorb.png";
+
+let poke101 = document.createElement("img");
+poke101.value = "images/electrode.png";
+
+let poke102 = document.createElement("img");
+poke102.value = "images/exeggcute.png";
+
+let poke103 = document.createElement("img");
+poke103.value = "images/exeggutor.png";
+
+let poke104 = document.createElement("img");
+poke104.value = "images/cubone.png";
+
+let poke105 = document.createElement("img");
+poke105.value = "images/marowak.png";
+
+let poke106 = document.createElement("img");
+poke106.value = "images/hitmonlee.png";
+
+let poke107 = document.createElement("img");
+poke107.value = "images/hitmonchan.png";
+
+let poke108 = document.createElement("img");
+poke108.value = "images/lickitung.png";
+
+let poke109 = document.createElement("img");
+poke109.value = "images/koffing.png";
+
+let poke110 = document.createElement("img");
+poke110.value = "images/weezing.png";
+
+let poke111 = document.createElement("img");
+poke111.value = "images/rhyhorn.png";
+
+let poke112 = document.createElement("img");
+poke112.value = "images/rhydon.png";
+
+let poke113 = document.createElement("img");
+poke113.value = "images/chansey.png";
+
+let poke114 = document.createElement("img");
+poke114.value = "images/tangela.png";
+
+let poke115 = document.createElement("img");
+poke115.value = "images/kangaskhan.png";
+
+let poke116 = document.createElement("img");
+poke116.value = "images/horsea.png";
+
+let poke117 = document.createElement("img");
+poke117.value = "images/seadra.png";
+
+let poke118 = document.createElement("img");
+poke118.value = "images/goldeen.png";
+
+let poke119 = document.createElement("img");
+poke119.value = "images/seaking.png";
+
+let poke120 = document.createElement("img");
+poke120.value = "images/staryu.png";
+
+let poke121 = document.createElement("img");
+poke121.value = "images/starmie.png";
+
+let poke122 = document.createElement("img");
+poke122.value = "images/mrmime.png";
+
+let poke123 = document.createElement("img");
+poke123.value = "images/scyther.png";
+
+let poke124 = document.createElement("img");
+poke124.value = "images/jynx.png";
+
+let poke125 = document.createElement("img");
+poke125.value = "images/electabuzz.png";
+
+let poke126 = document.createElement("img");
+poke126.value = "images/magmar.png";
+
+let poke127 = document.createElement("img");
+poke127.value = "images/pinsir.png";
+
+let poke128 = document.createElement("img");
+poke128.value = "images/tauros.png";
+
+let poke129 = document.createElement("img");
+poke129.value = "images/magikarp.png";
+
+let poke130 = document.createElement("img");
+poke130.value = "images/gyarados.png";
+
+let poke131 = document.createElement("img");
+poke131.value = "images/lapras.png";
+
+let poke132 = document.createElement("img");
+poke132.value = "images/ditto.png";
+
+let poke133 = document.createElement("img");
+poke133.value = "images/eevee.png";
+
+let poke134 = document.createElement("img");
+poke134.value = "images/vaporeon.png";
+
+let poke135 = document.createElement("img");
+poke135.value = "images/jolteon.png";
+
+let poke136 = document.createElement("img");
+poke136.value = "images/flareon.png";
+
+let poke137 = document.createElement("img");
+poke137.value = "images/porygon.png";
+
+let poke138 = document.createElement("img");
+poke138.value = "images/omanyte.png";
+
+let poke139 = document.createElement("img");
+poke139.value = "images/omastar.png";
+
+let poke140 = document.createElement("img");
+poke140.value = "images/kabuto.png";
+
+let poke141 = document.createElement("img");
+poke141.value = "images/kabutops.png";
+
+let poke142 = document.createElement("img");
+poke142.value = "images/aerodactyl.png";
+
+let poke143 = document.createElement("img");
+poke143.value = "images/snorlax.png";
+
+let poke144 = document.createElement("img");
+poke144.value = "images/articuno.png";
+
+let poke145 = document.createElement("img");
+poke145.value = "images/zapdos.png";
+
+let poke146 = document.createElement("img");
+poke146.value = "images/moltres.png";
+
+let poke147 = document.createElement("img");
+poke147.value = "images/dratini.png";
+
+let poke148 = document.createElement("img");
+poke148.value = "images/dragonair.png";
+
+let poke149 = document.createElement("img");
+poke149.value = "images/dragonite.png";
+
+let poke150 = document.createElement("img");
+poke150.value = "images/mewtwo.png";
+
+let poke151 = document.createElement("img");
+poke151.value = "images/mew.png";
 
 
-const allPoke = [poke1, poke2, poke3, poke4, poke5, poke6, poke7, poke8, poke9];
+const allPoke = [poke1, poke2, poke3, poke4, poke5, poke6, poke7, poke8, poke9, poke10, poke11, poke12, poke13, poke14, poke15, poke16, poke17, poke18, poke19,
+    poke20, poke21, poke22, poke23, poke24, poke25, poke26, poke27, poke28, poke29, poke30, poke31, poke32, poke33, poke34, poke35, poke36, poke37,
+    poke38, poke39, poke40, poke41, poke42, poke43, poke44, poke45, poke46, poke47, poke48, poke49, poke50, poke51, poke52, poke53, poke54, poke55,
+    poke56, poke57, poke58, poke59, poke60, poke61, poke62, poke63, poke64, poke65, poke66, poke67, poke68, poke69, poke70, poke71, poke72, poke73,
+    poke74, poke75, poke76, poke77, poke78, poke79, poke80, poke81, poke82, poke83, poke84, poke85, poke86, poke87, poke88, poke89, poke90, poke91,
+    poke92, poke93, poke94, poke95, poke96, poke97, poke98, poke99, poke100, poke101, poke102, poke103, poke104, poke105, poke106, poke107, poke108,
+    poke109, poke110, poke111, poke112, poke113, poke114, poke115, poke116, poke117, poke118, poke119, poke120, poke121, poke122, poke123, poke124,
+    poke125, poke126, poke127, poke128, poke129, poke130, poke131, poke132, poke133, poke134, poke135, poke136, poke137, poke138, poke139, poke140,
+    poke141, poke142, poke143, poke144, poke145, poke146, poke147, poke148, poke149, poke150, poke151];
